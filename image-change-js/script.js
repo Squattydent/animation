88c51709c01416233569deed2109c5f20c838e0b
mussaimo <okemwamoses@gmail.com>
2016-04-12 13:33:11 +0300

    optimized code

--- image-change-js/script.js
+++ image-change-js/script.js
@@ -1,13 +1,3 @@
-function changeimage() {
-    var image = document.getElementById('myImage');
-    if (image.src.match("bulbon")) {
-        image.src = "bulboff.gif";
-    } else {
-        image.src = "bulbon.gif";
-    }
-}
-
-// from jsfiddle
 jQuery(document).ready(function ($) {
     $('#cmn-toggle-1').click(
         function () {
