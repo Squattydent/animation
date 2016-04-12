b67c286b36b301f53fdd8b22980691f12c50fdd9
mussaimo <okemwamoses@gmail.com>
2016-04-12 12:06:27 +0300

    added second realistic bulb

--- image-change-js/script.js
+++ image-change-js/script.js
@@ -1,8 +1,32 @@
 function changeimage() {
     var image = document.getElementById('myImage');
-    if (image.src.match("true")) {
-        image.src = "moringa-white.png";
+    if (image.src.match("bulbon")) {
+        image.src = "bulboff.gif";
     } else {
-        image.src = "true.jpg";
+        image.src = "bulbon.gif";
     }
 }
+
+// from jsfiddle
+
+//function cubeswitch() {
+//    var image = document.getElementById("light-bulb2");
+//    if (image.)
+//        if ($('.cube-switch').hasClass('active')) {
+//            $('.cube-switch').removeClass('active');
+//            $('#light-bulb2').css({
+//                'opacity': '0'
+//            });
+//        } else {
+//            $('.cube-switch').addClass('active');
+//            $('#light-bulb2').css({
+//                'opacity': '1'
+//            });
+//        }
+//}
+
+$('cmn-toggle').click(function () {
+
+    $("#light-bulb2").toggleClass('on');
+
+});
